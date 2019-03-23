164280cfd5ac961905d6d45957eaa9dc36bf3eee
顾咏 <zhijiang.nzj@alibaba-inc.com>
2019-03-23 17:03:28 +0800

    Update Apis

--- V20171204/Api.php
+++ V20171204/Api.php
@@ -2,7 +2,26 @@
 
 namespace AlibabaCloud\Actiontrail\V20171204;
 
+use AlibabaCloud\ApiResolverTrait;
 use AlibabaCloud\Rpc;
+
+/**
+ * Resolve Api based on the method name.
+ *
+ * @method DescribeRegions describeRegions(array $options = [])
+ * @method LookupEvents lookupEvents(array $options = [])
+ * @method UpdateTrail updateTrail(array $options = [])
+ * @method StopLogging stopLogging(array $options = [])
+ * @method GetTrailStatus getTrailStatus(array $options = [])
+ * @method DescribeTrails describeTrails(array $options = [])
+ * @method StartLogging startLogging(array $options = [])
+ * @method CreateTrail createTrail(array $options = [])
+ * @method DeleteTrail deleteTrail(array $options = [])
+ */
+class ActiontrailApiResolver
+{
+    use ApiResolverTrait;
+}
 
 class V20171204Rpc extends Rpc
 {
